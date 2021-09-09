4cefa56f3f1792be55710ce9a5ab64139206f115
Roni Pereira <roni.pereira@cwi.com.br>
2021-09-09 15:20:49 -0300

    feat(Startup): Conteúdos do vídeo: Steps HttpRequest - Parte 1.

---
+++ Startup.cs
@@ -0,0 +1,21 @@
+﻿using BoDi;
+using TechTalk.SpecFlow;
+
+namespace Cwi.TreinamentoTesteAutomatizado
+{
+    [Binding]
+    public class Startup
+    {
+        private readonly IObjectContainer ObjectContainer;
+        public Startup(IObjectContainer objectContainer)
+        {
+            ObjectContainer = objectContainer;
+        }
+
+        [BeforeScenario]
+        public void DependecyInjection()
+        {
+
+        }
+    }
+}
